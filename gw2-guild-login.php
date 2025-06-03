c7225155b8b9abb08089867f6101ee31db3f2687
AlteredM1nd <forever.twisted@gmail.com>
2025-06-03 09:44:28 -0500

    Updates plugin version to 2.6.4
    Increments the plugin version from 2.6.2 to 2.6.4

--- gw2-guild-login.php
+++ gw2-guild-login.php
@@ -10,7 +10,7 @@ use GW2GuildLogin\GW2_Login_Shortcode;
  * Plugin Name:       GW2 Guild Login
  * Plugin URI:        https://github.com/AlteredM1nd/gw2-guild-login
  * Description:       Allows users to log in using their GW2 API key to verify guild membership with WordPress user integration.
- * Version:           2.6.2
+ * Version:           2.6.4
  * Author:            AlteredM1nd
  * Author URI:        https://github.com/AlteredM1nd
  * License:           GPL-2.0+
